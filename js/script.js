70beca4fafcc0d6de4434de218848f6e6c563f7d
inessaoganyan21 <34602961+inessaoganyan21@users.noreply.github.com>
2021-03-30 09:56:28 +0400

    duration

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 			isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
 			windowReady = false,
 			isNoviBuilder = false,
-			pageTransitionDuration = 500,
+			pageTransitionDuration = 50,
 
 			plugins = {
 				bootstrapTooltip:        $("[data-toggle='tooltip']"),
